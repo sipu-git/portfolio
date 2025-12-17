f60a8b7c3ec389a3be567b0dc61aba6044735225
Sipu-982 <sipurana38@gmail.com>
2025-12-17 14:50:49 +0530

    skill section C langauge icon updated

--- components/SkillSection/page.tsx
+++ components/SkillSection/page.tsx
@@ -86,7 +86,7 @@ export default function SkillSection() {
 
         <div className="flex justify-between">
           <Circle ref={typescriptRef} src="/coding-logos/typescript.png" alt="TypeScript" />
-          <Circle ref={cRef} src="/coding-logos/c.png" alt="c" />
+          <Circle ref={cRef} src="/coding-logos/C.png" alt="clang" />
         </div>
 
         <div className="flex justify-between">
